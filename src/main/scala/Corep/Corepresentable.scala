2178898661929567cc821716291b270254bbe4c6
Sagi Fogel <sagi.f@syte.ai>
2021-05-09 00:55:16 +0300

    addition of external Aux type for Corepresentable.Aux/Cosieve

--- src/main/scala/Corep/Corepresentable.scala
+++ src/main/scala/Corep/Corepresentable.scala
@@ -1,36 +1,32 @@
 package Corep
 
+import cats.Functor
 import cats.arrow.Profunctor
 
 import scala.annotation.implicitNotFound
 
 @implicitNotFound("Could not find an instance of Corepresentable[${P}]")
-trait Corepresentable[P[_, _], F[_]] extends Cosieve[P, F] with Costrong[P] {
+trait Corepresentable[P[_, _]] extends Cosieve[P, Corep.Aux[P]#Corepresentation] with Costrong[P] {
   def P: Profunctor[P]
 
-  type Corepresentation
-
-  def cotabulate[A, B](f: Corepresentation => B): P[A, B]
+  def cotabulate[A, B](f: Corep.Aux[P]#Corepresentation[A] => B): P[A, B]
 }
 
 abstract class CorepresentableInstances {
-  implicit def corepresentableCostar[P[_, _], F[_]](implicit ev: Profunctor[Costar[F, *, *]]): Corepresentable.Aux[Costar[F, *, *], F] =
-    new Corepresentable[Costar[F, *, *], F] {
-      override def P: Profunctor[Costar[F, *, *]] = ev
-      override type Corepresentation = F
-      override def cotabulate[A, B](f: Corepresentation => B): Costar[F, A, B] = ???
+  implicit def corepresentableCostar[P[_, _], F[_] : Functor](implicit ev: Profunctor[Costar[F, *, *]]): Corepresentable[Costar[F, *, *]] = new Corepresentable[Costar[F, *, *]] {
+    override def P: Profunctor[Costar[F, *, *]] = ???
 
-      override def cosieve[A, B](pab: Costar[F, A, B])(fa: F[A]): B = ???
+    override def cotabulate[A, B](f: Corep.Aux[Costar[F, *, *]]#Corepresentation[A] => B): Costar[F, A, B] = ???
 
-      override def dimap[A, B, C, D](fab: Costar[F, A, B])(f: C => A)(g: B => D): Costar[F, C, D] = ???
+    override def cosieve[A, B](pab: Costar[F, A, B])(fa: Corep.Aux[Costar[F, *, *]]#Corepresentation[A]): B = ???
 
-      override def map[A, B](fa: F[A])(f: A => B): F[B] = ???
-    }
+    override def dimap[A, B, C, D](fab: Costar[F, A, B])(f: C => A)(g: B => D): Costar[F, C, D] = fab.dimap(f)(g)
+
+    override def map[A, B](fa: Corep.Aux[Costar[F, *, *]]#Corepresentation[A])(f: A => B): Corep.Aux[Costar[F, *, *]]#Corepresentation[B] = ???
+  }
 }
 
 object Corepresentable extends CorepresentableInstances {
-  type Aux[P[_, _], F[_]] = Corepresentable[P, F] { type Corepresentation = F }
-
   /** summon an instance of [[Corepresentable]] for `P` */
-  @inline def apply[P[_, _], F[_]](implicit ev: Corepresentable[P, F]): Corepresentable.Aux[P, ev.Corepresentation] = ev
+  @inline def apply[P[_, _]](implicit ev: Corepresentable[P]): Corepresentable[P] = ev
 }
